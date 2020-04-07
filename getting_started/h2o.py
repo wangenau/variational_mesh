00535d51843edcdb998091956935cf6070a2bf2c
Wanja Schulze <wangenau@protonmail.com>
2020-04-07 16:15:10 +0200

    Use PW correlation functional
    - also make sure open gets properly closed, plus some small changes

--- getting_started/h2o.py
+++ getting_started/h2o.py
@@ -3,8 +3,8 @@ from pyscf import gto, dft
 
 h2o = gto.Mole()
 h2o.atom = 'O 0 0 0; H 0 1 0; H 0 0 1'
-h2o.basis = 'sto-3g'
-#h2o.basis = 'dzp'
+#h2o.basis = 'sto-3g'
+h2o.basis = 'dzp'
 h2o.build()
 # same as:
 # h2o = gto.Mole()
@@ -13,7 +13,7 @@ h2o.build()
 # h2o = gto.M(atom='O 0 0 0; H 0 1 0; H 0 0 1', basis='sto-3g')
 
 mf = dft.RKS(h2o)
-mf.xc = 'lda'
+mf.xc = 'lda,pw'
 mf.verbose = 4
 mf.kernel()
 # same as:
--- getting_started/plot_energies.py
+++ getting_started/plot_energies.py
@@ -5,11 +5,12 @@ from matplotlib.ticker import FormatStrFormatter
 def get_energies(file):
     '''Get energies per iteration from an output file.'''
     energies = []
-    for line in open(file):
-        tmp = line.rstrip().split(" E= ")
-        if len(tmp) == 2:
-            tmp = tmp[1].split('  ')[0]
-            energies.append(float(tmp))
+    with open(file,'r') as file:
+        for line in file:
+            tmp = line.rstrip().split(' E= ')
+            if len(tmp) >= 2:
+                tmp = tmp[1].split('  ')[0]
+                energies.append(float(tmp))
     return energies
 
 
@@ -18,10 +19,10 @@ def main():
     e_dzp = get_energies('h2o_dzp.out')
     fig, ax = plt.subplots()
     ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
-    plt.plot(e_sto3g, label="STO-3G")
-    plt.plot(e_dzp, label="DZP")
-    plt.xlabel("iteration")
-    plt.ylabel("energies [Ha]")
+    plt.plot(e_sto3g, label='STO-3G')
+    plt.plot(e_dzp, label='DZP')
+    plt.xlabel('iteration')
+    plt.ylabel('energies [Ha]')
     plt.legend(loc='center right')
     plt.show()
 
