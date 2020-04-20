8cc8b3e62538bf8f9b7210b8b10f69f6751f302a
Wanja Schulze <wangenau@protonmail.com>
2020-04-20 19:35:01 +0200

    Add basis set limit calulator for the total energy

--- getting_started/h2o.py
+++ getting_started/h2o.py
@@ -8,7 +8,7 @@ h2o.basis = 'sto-3g'
 h2o.build()
 # same as:
 # h2o = gto.Mole()
-# h2o.build(atom='O 0, 0, 0; H 0, 1, 0; H 0, 0, 1'', basis='sto-3g')
+# h2o.build(atom='O 0, 0, 0; H 0, 1, 0; H 0, 0, 1', basis='sto-3g')
 # or:
 # h2o = gto.M(atom='O 0, 0, 0; H 0, 1, 0; H 0, 0, 1', basis='sto-3g')
 
