a90bccf0b22fa003d46f69ed12d62e42fa384183
Wanja Schulze <wangenau@protonmail.com>
2020-05-01 19:34:11 +0200

    Rename main file, also fix COM function
    - also make it work with arbitrary dimensions

--- variational_mesh.py
+++ variational_mesh.py
@@ -3,18 +3,19 @@ from pyscf import dft, gto
 
 
 def center_of_mass(coords, weights=None):
+    '''Calculate the center of mass for a list of points and their weights.'''
     if not isinstance(weights, (list, np.ndarray)):
         weights = [1] * len(coords)
-    com = [0, 0, 0]
+    com = np.full(len(coords[0]), 0)
     for i in range(len(coords)):
         com += coords[i] * weights[i]
     return com / sum(weights)
 
 
 def main():
-    h2o = gto.M(atom='O 0, 0, 0; H 0, 1, 0; H 0, 0, 1', basis='sto-3g')
-    atm_coords = np.asarray(h2o.atom_coords())
-    atm_masses = np.asarray(h2o.atom_mass_list())
+    mol = gto.M(atom='O 0, 0, 0; H 0, 1, 0; H 0, 0, 1', basis='sto-3g')
+    atm_coords = np.asarray(mol.atom_coords())
+    atm_masses = np.asarray(mol.atom_mass_list())
     atm_com = center_of_mass(atm_coords, atm_masses)
     print(atm_com)
 
