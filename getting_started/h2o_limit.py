7244a3aa7d49eac72c20b3372616c5e5bdf4bfbc
Wanja Schulze <wangenau@protonmail.com>
2020-05-03 16:17:16 +0200

    Small lint fixes
    - also fix filenames

--- getting_started/h2o_limit.py
+++ getting_started/h2o_limit.py
@@ -11,7 +11,7 @@ sets = len(basis)
 # calculate energy per basis set
 e = []
 for i in range(len(basis)):
-    print('[{}/{}]'.format(i+1, sets))
+    print('[{}/{}]'.format(i + 1, sets))
     tmp = gto.Mole()
     tmp.atom = 'O 0, 0, 0; H 0, 1, 0; H 0, 0, 1'
     tmp.basis = basis[i]
@@ -20,13 +20,13 @@ for i in range(len(basis)):
 
 # approximate the basis set limit y
 f = lambda x, y, a, b: y + a * np.exp(-b * x)
-p, _ = curve_fit(f, np.arange(1, sets+1), e)
+p, _ = curve_fit(f, np.arange(1, sets + 1), e)
 print('Basis set limit: {} Ha'.format(p[0]))
 
 # plot the calculated energies and the fitted curve
 plt.figure()
 plt.xlabel('basis functions per atomic orbital')
 plt.ylabel('energies [Ha]')
-plt.plot(np.arange(1, sets+1), e)
+plt.plot(np.arange(1, sets + 1), e)
 plt.plot(np.arange(1, sets, 0.1), f(np.arange(1, sets, 0.1), *p))
 plt.show()
