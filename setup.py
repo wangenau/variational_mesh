e99b6f27224ca6926cd25ed3cef2a53f27d09aa5
Wanja Schulze <wangenau@protonmail.com>
2020-11-02 15:57:54 +0100

    Version: 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='var_mesh',
-    version='0.0.1',
+    version='1.0.0',
     description='Variational Mesh',
     long_description=long_description,
     long_description_content_type='text/markdown',
