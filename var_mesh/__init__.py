5c9a4a449ea0fca932fd0d1407979025bcc6cd07
Wanja Schulze <wangenau@protonmail.com>
2021-01-27 23:55:38 +0100

    Add version info to module

--- var_mesh/__init__.py
+++ var_mesh/__init__.py
@@ -2,3 +2,4 @@ from var_mesh.gen_mesh import ang_grids, var_mesh
 from var_mesh.helpers import plot_mesh_2d, plot_mesh_3d
 
 __all__ = ['ang_grids', 'plot_mesh_2d', 'plot_mesh_3d', 'var_mesh']
+__version__ = '1.0.1'
