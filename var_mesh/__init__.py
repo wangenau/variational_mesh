c658e8b5e21fc1f8c71380e034803676d7bd8a5e
Wanja Schulze <wangenau@protonmail.com>
2021-12-13 17:47:18 +0100

    init: Fix pep8 warning

--- var_mesh/__init__.py
+++ var_mesh/__init__.py
@@ -3,4 +3,5 @@ from var_mesh.gen_mesh import ang_grids, var_mesh
 from var_mesh.helpers import plot_mesh_2d, plot_mesh_3d
 from var_mesh.version import __version__
 
-__all__ = ['ang_grids', 'plot_mesh_2d', 'plot_mesh_3d', 'var_mesh']
+__all__ = ['ang_grids', 'plot_mesh_2d', 'plot_mesh_3d', 'var_mesh',
+           '__version__']
